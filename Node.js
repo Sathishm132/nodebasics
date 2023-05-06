037c893eec2a77862d8153869788723517f0b4b4
Your Name <you@example.com>
2023-05-06 10:27:33 +0530

    render message from server file

--- Node.js
+++ Node.js
@@ -1,1 +1,11 @@
 console.log("hello")
+let fruits=["apple","mango","","lemon","","orange",""]
+let newf=fruits.map((fruit)=>{
+    if(fruit){
+        return fruit
+    }
+    else{
+        return "empytystring"
+    }
+})
+console.log(newf);
--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 const http=require("http")
+const fs=require("fs");
 const server=http.createServer((req,res)=>{
    
     const url=req.url;
+    const method=req.method;
     if(url==="/"){
+        fs.readFile('message.txt',{encoding:"utf-8"},(err,data)=>{
+            if(err){
+                console.log(err)
+            }
         res.write('<html>')
         res.write('<head><title>enter</title></head>')
-        res.write('<body><h1>welcome </h1></body>')
+        res.write(`<body>${data}</body>`)
+        res.write('<body><Form action=/message method=POST><input type="text" name="message"></input><button type=submit>send</button></Form></body>')
         res.write('</html>')
         console.log("hello")
-        res.end()
+        return res.end()
+
+        })
+        
     }
-    if(url==="/home"){
-        res.write('<html>')
-        res.write('<head><title>enter</title></head>')
-        res.write('<body><h1>welcome home </h1></body>')
-        res.write('</html>')
-        console.log("hello")
-        res.end()
+    if(url==="/message"&&method==="POST"){
+        const body=[]
+        req.on("data",(chunck)=>{
+            body.push(chunck)
+        })
+         return req.on('end',()=>{
+            const pharseBody=Buffer.concat(body).toString()
+            const message=pharseBody.split("=")[1]
+            fs.writeFileSync('message.txt',message,()=>{
+                res.statusCode=302;
+                res.setHeader('Location','/')
+             return res.end()
+            })
+            console.log(pharseBody)
+        })
+      
+        
     }
+  
+        
+    
    
 })
 server.listen(4000)
